78ae1283c770676ca8f0e02dc93c1175fb24dbe0
Mingxin WANG <37770806+kzil88@users.noreply.github.com>
2018-07-26 09:11:26 +0800

    Add files via upload

---
+++ Init_Tick.py
@@ -0,0 +1,88 @@
+import datetime
+import tushare as ts
+import numpy as np
+import pymysql
+import re
+import time
+
+if __name__ == '__main__':
+    db = pymysql.connect(host='127.0.0.1', user='root', passwd='admin', db='future', charset='utf8')
+    cursor = db.cursor()
+    cons = ts.get_apis()
+    future_code = 'AG1905'
+
+    # 先清空最近一天的数据库记录
+    sql_init = "SET SQL_SAFE_UPDATES = 0"
+    cursor.execute(sql_init)
+    db.commit()
+    sql_max_dt = "select max(b.state_dt) from future_tick b where b.future_code = '%s'"%(future_code)
+    cursor.execute(sql_max_dt)
+    done_set_max = cursor.fetchall()
+    db.commit()
+    if len(done_set_max) > 0:
+        sql_delete = "delete a.* from future_tick a where a.state_dt = '%s' and a.future_code = '%s'" %(str(done_set_max[0][0]),future_code)
+        cursor.execute(sql_delete)
+        db.commit()
+
+    try:
+        temp_day = ts.bar(future_code, conn=cons, asset='X')
+        #temp_day = ts.tick(future_code,conn=cons,asset='X',date='2017-12-20')
+        date_seq = list(temp_day.index)
+        date_seq = [str(x)[:10] for x in date_seq][::-1]
+        print(date_seq)
+    except Exception as aa:
+        print('No DATA Code:')
+    for i in range(len(date_seq)):
+        sql_check = "select * from future_tick a where a.state_dt = '%s' and a.future_code = '%s'"%(date_seq[i],future_code)
+        cursor.execute(sql_check)
+        done_set_check = cursor.fetchall()
+        db.commit()
+        if len(done_set_check) > 0:
+            print(str(date_seq[i]) + '   Already Exists')
+            continue
+        cnt_try = 0
+        c_len = -1
+        while True:
+            try:
+                temp_price = ts.tick(future_code,conn=cons,asset='X',date=date_seq[i])
+                #print(temp_price)
+                c_len = temp_price.shape[0]
+                if c_len >= 0:
+                    break
+            except Exception as bb:
+                cnt_try += 1
+                cons = ts.get_apis()
+                print(cnt_try)
+                time.sleep(17)
+                continue
+
+        print(str(date_seq[i]) + '  Tick Len : ' + str(c_len))
+        if c_len > 0:
+            for j in range(c_len):
+                resu = list(temp_price.ix[j])
+                state_dt = str(resu[0])[:10]
+                state_ts = int(time.mktime(time.strptime(str(resu[0]), "%Y-%m-%d %H:%M:%S")))
+                state_time = datetime.datetime.fromtimestamp(int(time.mktime(time.strptime(str(resu[0]), "%Y-%m-%d %H:%M:%S")))).strftime("%Y-%m-%d-%H-%M-%S")
+                future_price = float(resu[1])
+                future_vol = int(resu[2])
+                deal_type = resu[4]
+
+                sql_select = "select * from future_tick a where a.future_code = '%s' and a.state_time = '%s'"%(future_code,state_time)
+                cursor.execute(sql_select)
+                done_set_select = cursor.fetchall()
+                db.commit()
+                if len(done_set_select) > 0:
+                    sql_update = "update future_tick w set w.future_vol = w.future_vol + %i"%(future_vol)
+                    cursor.execute(sql_update)
+                    db.commit()
+                else:
+                    sql_insert = "insert into future_tick(state_dt,state_time,future_code,future_price,future_vol,deal_type,state_ts)values('%s','%s','%s','%.2f','%i','%s','%i')"%(state_dt,state_time,future_code,future_price,future_vol,deal_type,state_ts)
+                    cursor.execute(sql_insert)
+                    db.commit()
+
+
+    print('All Finished!')
+    db.close()
+
+
+
